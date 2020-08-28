861e98bd48a91cf3223885ff95a16242aa302de8
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-28 08:46:32 +0100

    add triangle and refactor to use named fields

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -13,19 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{13, 5}, 65},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 5, Height: 13}, hasArea: 65.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
 	}
+
 }
