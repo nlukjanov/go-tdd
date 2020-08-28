ae8007d17f8994e850d9537bed59fe8e41782fa5
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-28 09:54:27 +0100

    add deposit and withdraw

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
