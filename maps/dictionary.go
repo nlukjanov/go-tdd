d12330cf270eb34ea25e4d9bd10faae0cacba71f
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-29 08:17:31 +0100

    add basic dictionary and search

---
+++ maps/dictionary.go
@@ -0,0 +1,11 @@
+package main
+
+type Dictionary map[string]string
+
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
