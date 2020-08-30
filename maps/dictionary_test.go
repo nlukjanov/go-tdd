7971ea0dfa6cf908a76fa5c79894e882ece9e5ee
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-30 11:37:51 +0100

    add update word in a dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -41,6 +41,29 @@ func TestAdd(t *testing.T) {
 		assertDefinition(t, dictionary, word, definition)
 	})
 }
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		newDefinition := "new definition"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
 	t.Helper()
